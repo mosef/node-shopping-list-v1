f970c3e0759649d4235c3702ae6533ad5b3c3cdc
Mosef Asad <mosefasad@gmail.com>
2017-10-11 22:18:19 -0400

    added recipe endpoint and response for recipe GET request

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const {ShoppingList} = require('./models');
+const {Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -17,13 +18,14 @@ app.use(morgan('common'));
 ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
-
+Recipes.create('chocolate milk', ['cocoa','milk','sugar']);
+Recipes.create('strawberry shake', ['strawberries','bananas','ice']);
+Recipes.create('vanilla float', ['vanilla icecream','root beer','sugar']);
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
-
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
 });
